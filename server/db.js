fcfd8394e4311e1fb15a979af92c19e07a5e54d6
Sachin Singh <akasachin45@gmail.com>
2026-07-12 17:20:34 +0530

    hii

--- server/db.js
+++ server/db.js
@@ -5,13 +5,16 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
+    console.log(err);
   } else {
-    console.log("MySQL Connected");
+    console.log("Database Connected");
   }
 });
 
