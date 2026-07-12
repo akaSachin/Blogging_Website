9416327c8b9de2ca895927090ea41993f6cfca30
Sachin Singh <akasachin45@gmail.com>
2026-07-12 17:45:01 +0530

    t

--- server/db.js
+++ server/db.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Database Connected");
-  }
+db.connect((err)=>{
+    if(err){
+        console.log("Database connection failed");
+        console.log(err);
+    }
+    else{
+        console.log("Database connected");
+    }
 });
 
-module.exports = db;
+module.exports=db;
